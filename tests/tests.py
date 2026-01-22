a57f84354ea8ca461fb7911edda496e24b19858d
Petr Freyburg <pfreybur@redhat.com>
2026-01-22 09:57:29 +0100

    add test_run_image

--- tests/tests.py
+++ tests/tests.py
@@ -5,11 +5,17 @@ import unittest
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from script import build_and_run_dockerfile, run_image, REPO_WORKDIR
+from script import build_and_run_dockerfile, run_image, scan_dockerfile, REPO_WORKDIR
 
 client = docker.from_env() 
 
 class TestPackageScanner(unittest.TestCase):
+    def test_scan_dockerfile(self):
+        desired_images = ['image_a', 'image_b', 'image_c:latest']
+        images = scan_dockerfile("tests/data/Dockerfile")
+        
+        self.assertEqual(desired_images, images)
+
     def test_run_image(self):
         x = run_image(client, "redhat/ubi8-minimal", "rpm", "grep")
         self.assertTrue(x.startswith("grep"))
