1813790056482e3ef0fa4a5704ea4cf7fa93b69a
Petr Freyburg <pfreybur@redhat.com>
2026-01-22 09:38:43 +0100

    add test

--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,21 @@
 import docker
 import os
-import pathlib
 import sys
 import unittest
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from script import build_and_run_dockerfile, REPO_WORKDIR
+from script import build_and_run_dockerfile, run_image, REPO_WORKDIR
+
+client = docker.from_env() 
 
 class TestPackageScanner(unittest.TestCase):
+    def test_run_image(self):
+        x = run_image(client, "redhat/ubi8-minimal", "rpm", "grep")
+        self.assertTrue(x.startswith("grep"))
+
     def test_build_and_run_dockerfile(self):
-        client = docker.from_env() 
         dockerfile = REPO_WORKDIR+"/Dockerfile"
 
         x = build_and_run_dockerfile(client, "tests/"+REPO_WORKDIR, dockerfile, "pip", "requests")
         self.assertEqual("requests==2.32.5", x.replace("\n", ""))
-    
-if __name__ == '__main__':
-    unittest.main()
